f081023fc6064bc7a768ce0eda0563d3042a7186
Rajee <Rajee@LAPTOP-T2NOHGEO>
2021-08-13 11:59:16 +0530

    Category->Clothes selected

--- CucumberJavaselenium/src/test/java/Stepdefgluecode/Homepage.java
+++ CucumberJavaselenium/src/test/java/Stepdefgluecode/Homepage.java
@@ -1,8 +1,12 @@
 package Stepdefgluecode;
 
 import io.cucumber.java.en.Given;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,45 +25,58 @@ public class Homepage {
 		System.out.println("Launch Page");
 		WebDriverManager.chromedriver().setup();
 		
+		
 		 driver=new ChromeDriver();
+		 WebDriverWait _wait = new WebDriverWait(driver, 50000);
 				driver.manage().window().maximize();			
+				//driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 	       driver.get("https://testscriptdemo.com"); // Opening webpage
+	       //driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+	
 	       
-	       WebDriverWait _wait = new WebDriverWait(driver, 10);
+	       Timeouts implicitlyWait = driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	       // findElement() will try to identify element till 5 secs
+	       WebElement n=driver.findElement(By.xpath(" //*[@id=\"cc-window\"]/div[5]/a[1]"));
+	       n.click();
 	       
-	       //
-	    
-
-	       //_wait.Until(d => d.FindElement(By.Id("Id_Your_UIElement"));
-	       
+	       //driver.findElement(By.xpath(" //*[@id=\"cc-window\"]/div[5]/a[1]")).click();
+	     	       
 	       // adding four elements to wishlist
-	       _wait = new WebDriverWait(driver, 2000000);
-	       //WebDriverWait(driver, 60).until(driver.findElement(By.xpath(" //*[@id=\"cc-window\"]/div[5]/a[1]")).)
-	       driver.findElement(By.xpath(" //*[@id=\"cc-window\"]/div[5]/a[1]")).click();
-	       _wait = new WebDriverWait(driver, 200000);
-	       
+	       driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	       //Accept All cookies
+	       
+	       _wait = new WebDriverWait(driver, 5000000);
+	       
+	       
+	       
 	       driver.findElement(By.xpath("//*[@id='site-content']/div/div/div/div/section[4]/div/div/div/div/div/div/div/ul/li[2]/div/div[2]/div/div/a")).click();
 	       System.out.println("Product1 added");
-	       _wait = new WebDriverWait(driver, 20000);
+	       _wait = new WebDriverWait(driver, 2000000);
+	       Timeouts implicitlyWait2 = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	       driver.findElement(By.xpath("//*[@id='site-content']/div/div/div/div/section[4]/div/div/div/div/div/div/div/ul/li[1]/div/div[2]/div/div/a")).click();
 	       System.out.println("Product2 added");
-	        _wait = new WebDriverWait(driver, 20000);
+	        _wait = new WebDriverWait(driver, 2000000);
+	        Timeouts implicitlyWait21 = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	        
 	        //
 	       driver.findElement(By.xpath("//*[@id=\"site-content\"]/div/div/div/div/section[4]/div/div/div/div/div/div/div/ul/li[5]/div/div[2]/div/div/a")).click();
 	      System.out.println("Product3 added");
-	       _wait = new WebDriverWait(driver, 20000);
+	       _wait = new WebDriverWait(driver, 2000000);
+	       Timeouts implicitlyWait3 = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	      driver.findElement(By.xpath("//*[@id=\"site-content\"]/div/div/div/div/section[4]/div/div/div/div/div/div/div/ul/li[3]/div/div[2]/div/div/a")).click();
 	       System.out.println("Product4 added");
-	       _wait = new WebDriverWait(driver, 20000);
+	       _wait = new WebDriverWait(driver, 2000000);
+	       Timeouts implicitlyWait4 = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	       System.out.println("Four items added to Wishlist");
+	       _wait = new WebDriverWait(driver, 2000000);
+	       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@When("user view mywhishlist")
 	public void user_view_mywhishlist() {
 	    // Viewing Wishlist
-		WebDriverWait _wait2 = new WebDriverWait(driver, 20000);
+		WebDriverWait _wait2 = new WebDriverWait(driver, 50000);
+		Timeouts implicitlyWait = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//*[@id=\'blog\']/div[3]/div[1]/div/div/div[3]/div[3]/a")).click();
 		System.out.println("Viewing Wishlist Table");
 	}
@@ -87,9 +104,17 @@ public class Homepage {
 	public void search_lower_price_product_in_relevance() {
 	    // Write code here that turns the phrase above into concrete actions
 		
-		_wait = new WebDriverWait(driver, 200000);
-		WebElement menu = driver.findElement(By.xpath("/html/body/div[2]/div[2]/nav/div/ul/li/a"));
-          menu.sendKeys("Clothing");
+		WebDriverWait _wait = new WebDriverWait(driver, 200000);
+		Timeouts implicitlyWait = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id=\"site-navigation\"]/div/ul/li/a")).click();
+		System.out.println("Category click");
+		////home page category/////html/body/div[3]/div[2]/nav/div/ul/li
+		driver.findElement(By.xpath("//*[@id=\"menu-item-302\"]/a")).click();
+		Timeouts implicitlyWait1 = driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		System.out.println("Clothes");
+		
+		
         //Thread.sleep(4000);
        // menu2.selectByVisibleText("Knightsbridge");
         //Thread.sleep(4000);
--- CucumberJavaselenium/src/test/resources/Features/Mywishlist.feature
+++ CucumberJavaselenium/src/test/resources/Features/Mywishlist.feature
@@ -1,18 +1,12 @@
-#This Feature file combines all when conditions for the given scenario  
-
+#This Feature file combines all when conditions for the given scenario
 @Smoketest
-Feature: Validate user able to add products into wish list , find lowest price item and 
-add into my cart 
-
-  
+Feature: Validate user able to add products into wish list , find lowest price item and
+  add into my cart
 
   Scenario Outline: Add four products into wishlist
     Given User add four products into wishlist
-    When user view mywhishlist
-        Then Find price of each wishlist item
-        When Search lower price product in Relevance
-        And Able to add in to cart
-        Then Verify myCart
-     
-
-      
+     When user view mywhishlist
+    #Then Find price of each wishlist item
+   When Search lower price product in Relevance
+  And Able to add in to cart
+    Then Verify myCart
