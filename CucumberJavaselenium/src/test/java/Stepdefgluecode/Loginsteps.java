816d985bc677323773b85af44ed43661abc8171e
Rajee <Rajee@LAPTOP-T2NOHGEO>
2021-08-13 08:47:25 +0530

    Test

--- CucumberJavaselenium/src/test/java/Stepdefgluecode/Loginsteps.java
+++ CucumberJavaselenium/src/test/java/Stepdefgluecode/Loginsteps.java
@@ -1,27 +1,46 @@
 package Stepdefgluecode;
 
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.*;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Loginsteps {
+	WebDriver driver;
+	 
 	
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("Testing");
+		WebDriverManager.chromedriver().setup();
+		
+		 driver=new ChromeDriver();
+				driver.manage().window().maximize();			
+	       driver.get("http://demo.guru99.com/v4/");
+	       
 	    	}
 
 	@When("user enters username and password")
-	public void user_enters_username_and_password() {
+	public void user_enters_username_and_password(String username,String password) {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("Testing");
+		
+		driver.findElement(By.name("uid")).sendKeys(username);					
+	       driver.findElement(By.name("password")).sendKeys(password);	
+	
 	}
 
 	@When("clicks on Login button")
 	public void clicks_on_login_button() {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("Testing");
+		
+		driver.findElement(By.name("btnLogin")).click();
+		//driver.findElement(By.tagName("Password must not be blank"));
+		//driver.close();
 	}
 
 	@Then("user navigating to landing page")
--- CucumberJavaselenium/src/test/resources/Features/Loginpage.feature
+++ CucumberJavaselenium/src/test/resources/Features/Loginpage.feature
@@ -2,11 +2,7 @@
 @Smoketest
 Feature: feature to test login
 
-  Scenario: Check login with valid values
-    Given user is on login page
-    When user enters username and password
-    And clicks on Login button
-    Then user navigating to landing page
+  
 
   Scenario Outline: Check login with valid values
     Given user is on login page
@@ -18,3 +14,4 @@ Feature: feature to test login
       | username | password |
       | user1    | pass2    |
       | user2    | pass1    |
+      
